41af36a931a83e74072dd71ca14126dc100f1c8b
Alex P <alex-xoxa@yandex.ru>
2022-04-14 22:52:52 -0700

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { states } from './components/Flags.js'
+
 
 function App() {
+
+  const [info, setInfo] = useState([]);
+  const [flag, setFlag] = useState(" ");
+  const [buttonClicked, setButtonCliked] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest");
+        const json = await response.json();
+        setInfo(json.data[Math.floor(Math.random() * json.data.length)]);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchData();
+    setButtonCliked(false)
+  }, [buttonClicked]);
+
+  useEffect(() => {
+    async function findState(data) {
+      let stateName = await states.find((el) => el.name === data["ID State"]);
+
+      setFlag(stateName.image);
+    }
+    findState(info);
+  }, [info]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(info).length ? (
+        <div className="state-info">
+          <p> State - {info.State} </p>
+          <p> Population (people) - {new Intl.NumberFormat("en-GB").format(info.Population)} </p>
+          <p> Year - {info.Year} </p>
+          <button className="button" onClick={() => setButtonCliked(true)}>
+            Next Random State
+          </button>
+        </div>
+      ) : (
+        "Loading"
+        )}
+        <div className="flag" style={{ backgroundImage: `url(${flag})` }}></div>
     </div>
   );
 }
---
+++ src/components/Flags.js
@@ -0,0 +1,268 @@
+export let states = [
+  {
+    name: "04000US01",
+    image: "/image/alabama.svg",
+  },
+  {
+    name: "04000US02",
+    image: "/image/alaska.svg",
+  },
+  {
+    name: "04000US04",
+    image: "/image/arizona.svg",
+  },
+  {
+    name: "04000US05",
+    image: "/image/arkansas.svg",
+  },
+  {
+    name: "04000US06",
+    image: "/image/california.svg",
+  },
+  {
+    name: "04000US08",
+    image: "/image/colorado.svg",
+  },
+  {
+    name: "04000US09",
+    image: "/image/connecticut.svg",
+  },
+  {
+    name: "04000US10",
+    image: "/image/delaware.svg",
+  },
+  {
+    name: "04000US11",
+    image: "/image/district-of-columbia.svg",
+  },
+  {
+    name: "04000US12",
+    image: "/image/florida.svg",
+  },
+  {
+    name: "04000US13",
+    image: "/image/georgia.svg",
+  },
+  {
+    name: "04000US15",
+    image: "/image/hawaii.svg",
+  },
+  {
+    name: "04000US16",
+    image: "/image/idaho.svg",
+  },
+  {
+    name: "04000US17",
+    image: "/image/illinois.svg",
+  },
+  {
+    name: "04000US18",
+    image: "/image/indiana.svg",
+  },
+  {
+    name: "04000US19",
+    image: "/image/iowa.svg",
+  },
+  {
+    name: "04000US20",
+    image: "/image/kansas.svg",
+  },
+  {
+    name: "04000US21",
+    image: "/image/kentucky.svg",
+  },
+  {
+    name: "04000US22",
+    image: "/image/louisiana.svg",
+  },
+  {
+    name: "04000US23",
+    image: "/image/maine.svg",
+  },
+  {
+    name: "04000US24",
+    image: "/image/maryland.svg",
+  },
+  {
+    name: "04000US25",
+    image: "/image/massachusetts.svg",
+  },
+  {
+    name: "04000US26",
+    image: "/image/michigan.svg",
+  },
+  {
+    name: "04000US27",
+    image: "/image/minnesota.svg",
+  },
+  {
+    name: "04000US28",
+    image: "/image/mississippi.svg",
+  },
+  {
+    name: "04000US29",
+    image: "/image/missouri.svg",
+  },
+  {
+    name: "04000US30",
+    image: "/image/montana.svg",
+  },
+  {
+    name: "04000US31",
+    image: "/image/nebraska.svg",
+  },
+  {
+    name: "04000US32",
+    image: "/image/nevada.svg",
+  },
+  {
+    name: "04000US33",
+    image: "/image/new-hampshire.svg",
+  },
+  {
+    name: "04000US34",
+    image: "/image/new-jersey.svg",
+  },
+  {
+    name: "04000US35",
+    image: "/image/new-mexico.svg",
+  },
+  {
+    name: "04000US36",
+    image: "/image/new-york.svg",
+  },
+  {
+    name: "04000US37",
+    image: "/image/north-carolina.svg",
+  },
+  {
+    name: "04000US38",
+    image: "/image/north-dakota.svg",
+  },
+  {
+    name: "04000US39",
+    image: "/image/ohio.svg",
+  },
+  {
+    name: "04000US40",
+    image: "/image/oklahoma.svg",
+  },
+  {
+    name: "04000US41",
+    image: "/image/oregon.svg",
+  },
+  {
+    name: "04000US42",
+    image: "/image/pennsylvania.svg",
+  },
+  {
+    name: "04000US44",
+    image: "/image/rhode-island.svg",
+  },
+  {
+    name: "04000US45",
+    image: "/image/south-carolina.svg",
+  },
+  {
+    name: "04000US46",
+    image: "/image/south-dakota.svg",
+  },
+  {
+    name: "04000US47",
+    image: "/image/tennessee.svg",
+  },
+  {
+    name: "04000US48",
+    image: "/image/texas.svg",
+  },
+  {
+    name: "04000US49",
+    image: "/image/utah.svg",
+  },
+  {
+    name: "04000US50",
+    image: "/image/vermont.svg",
+  },
+  {
+    name: "04000US51",
+    image: "/image/virginia.svg",
+  },
+  {
+    name: "04000US53",
+    image: "/image/washington.svg",
+  },
+  {
+    name: "04000US54",
+    image: "/image/west-virginia.svg",
+  },
+  {
+    name: "04000US55",
+    image: "/image/wisconsin.svg",
+  },
+  {
+    name: "04000US56",
+    image: "/image/wyoming.svg",
+  },
+  {
+    name: "04000US72",
+    image: "/image/puerto-rico.svg",
+  },
+];
+
+/*
+import alabama from "/image/alabama.svg";
+import alaska from "/image/alaska.svg";
+import americanSamoa from "/image/american-samoa.svg";
+import arizona from "/image/arizona.svg";
+import arkansas from "/image/arkansas.svg";
+import california from "/image/california.svg";
+import colorado from "/image/colorado.svg";
+import connecticut from "/image/connecticut.svg";
+import delaware from "/image/delaware.svg";
+import districtOfColumbia from "/image/district-of-columbia.svg";
+import florida from "/image/florida.svg";
+import georgia from "/image/georgia.svg";
+import guam from "/image/guam.svg";
+import hawaii from "/image/hawaii.svg";
+import idaho from "/image/idaho.svg";
+import illinois from "/image/illinois.svg";
+import indiana from "/image/indiana.svg";
+import iowa from "/image/iowa.svg";
+import kansas from "/image/kansas.svg";
+import kentucky from "/image/kentucky.svg";
+import louisiana from "/image/louisiana.svg";
+import maine from "/image/maine.svg";
+import marshall from "/image/marshall-islands.svg";
+import maryland from "/image/maryland.svg";
+import massachusetts from "/image/massachusetts.svg";
+import michigan from "/image/michigan.svg";
+import minnesota from "/image/minnesota.svg";
+import mississippi from "/image/mississippi.svg";
+import missouri from "/image/missouri.svg";
+import montana from "/image/montana.svg";
+import nebraska from "/image/nebraska.svg";
+import nevada from "/image/nevada.svg";
+import newHampshire from "/image/new-hampshire.svg";
+import newJersey from "/image/new-jersey.svg";
+import newMexico from "/image/new-mexico.svg";
+import newYork from "/image/new-york.svg";
+import northCarolina from "/image/north-carolina,svg";
+import northDakota from "/image/north-dakota.svg";
+import northemMarina from "/image/northern-mariana-islands,svg";
+import ohio from "/image/ohio.svg";
+import oklahoma from "/image/oklahoma.svg";
+import oregon from "/image/oregon.svg";
+import pennsylvania from "/image/pennsylvania.svg";
+import puertoRico from "/image/puerto-rico.svg";
+import rhoeIsland from "/image/rhode-island.svg";
+import southCarolina from "/image/south-carolina.svg";
+import southDakota from "/image/south-dakota.svg";
+import tennessee from "/image/tennessee.svg";
+import texas from "/image/texas.svg";
+import utah from "/image/utah.svg";
+import vermont from "/image/vermont.svg";
+import virginia from "/image/virginia.svg";
+import westVirginia from "/image/west-virginia.svg";
+import wisconsin from "/image/wisconsin.svg";
+import wyoming from "/image/wyoming.svg";
+*/
