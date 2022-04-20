2ff17f869d26f766395d4451f95d028c36b4eba6
Alex P <alex-xoxa@yandex.ru>
2022-04-19 21:38:49 -0700

    refactoring algorithm

--- src/App.js
+++ src/App.js
@@ -5,43 +5,44 @@ import { states } from './components/Flags.js'
 function App() {
 
   const [info, setInfo] = useState([]);
+  const [state, setState] = useState(0)
   const [flag, setFlag] = useState("");
-  const [buttonClicked, setButtonCliked] = useState(true);
 
   useEffect(() => {
-    if (buttonClicked === true) {
       async function fetchData() {
         try {
           const response = await fetch("https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest");
           const json = await response.json();
-          setInfo(json.data[Math.floor(Math.random() * json.data.length)]);
+          setInfo(json.data);
         } catch (e) {
           console.error(e);
         }
       }
       fetchData();
-      setButtonCliked(false);
+    }, []);
+
+     function randomState(data) {
+       setState([Math.floor(Math.random() * data.length)]);
     }
-  }, [buttonClicked]);
 
   useEffect(() => {
-     function findState(data) {
-      if (data.length !== 0) {
-        let stateName =  states.find((el) => el.name === data["ID State"]);
+    if (info[state]) {
+    function findState(data) {
+        let stateName = states.find((el) => el.name === data["ID State"]);
         setFlag(stateName.image);
       }
+      findState(info[state]);
     }
-    findState(info);
-  }, [info]);
+  }, [state, info]);
 
   return (
     <div className="App">
-      {Object.keys(info).length ? (
+      {Object.keys(info).length !== 0 ? (
         <div className="state-info">
-          <p> State - {info.State} </p>
-          <p> Population (people) - {new Intl.NumberFormat("en-GB").format(info.Population)} </p>
-          <p> Year - {info.Year} </p>
-          <button className="button" onClick={() => setButtonCliked(true)}>
+          <p> State - {info[state].State} </p>
+          <p> Population (people) - {new Intl.NumberFormat("en-GB").format(info[state].Population)} </p>
+          <p> Year - {info[state].Year} </p>
+          <button className="button" onClick={() => randomState(info)}>
             Next Random State
           </button>
         </div>
